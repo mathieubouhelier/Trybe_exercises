ef25218e335f9561b477168186eee4d44ba50a70
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-04-01 11:45:52 -0300

    bonus 4_3 finished

--- 4/4_3/bonus.js
+++ 4/4_3/bonus.js
@@ -1,33 +1,39 @@
-let index = 0;
+
+function numtoroman (num_romain){
+let index = "";
 let soma = 0;
 let tab_individual = [];
-let array_test = [100, 20 , 1, 1, 1];
-for (i=0; i<(array_test.length); i++){
-    console.log("i: ", i);
-    if (i == (array_test.length)){
-//Manque ce cas ultime index
-        break;
-    }
-    if (array_test[i]<array_test[i+1]){
-        index++;
-        console.log("soma first :", soma);
-        soma = soma + array_test[i+1]-array_test[i];
-        tab_individual[index] = soma;
-        console.log("index: ", index);
-        console.log("soma :", soma);
-        soma = 0;
-    }
-    if (array_test[i]>array_test[i+1]){
-        console.log("index: ", index);
-        console.log("soma :", soma);
-        index++;
-        tab_individual[index] = array_test[i];
-        
-    }
-    if(array_test[i] === array_test[i+1]){
-        console.log("index: ", index);
-        console.log("soma :", soma);
-        soma = soma + array_test[i];
-    }
+
+//let num_romain = "LXXVIII";
+let array_romain = [];
+let array_test = [];
+let test_valor;
+
+let object_roman = {I:1, V:5, X:10, L:50, C:100, D:500,M:1000};
+array_romain = num_romain.split("");
+
+for (i=0; i<(array_romain.length); i++){
+    index = array_romain[i];
+    array_test[i]= object_roman[index];
+   
 }
-console.log(tab_individual);
+for (i=0; i<(array_test.length); i++){
+    if (i == (array_test.length-1)){
+        soma = soma + array_test[i];
+        break
+        }
+        if (array_test[i]<array_test[i+1]){
+            soma = soma + array_test[i] * -1;
+
+        }
+        if (array_test[i]>=array_test[i+1]){
+            soma = soma + array_test[i];
+
+        }
+    }
+    console.log(soma);
+    return soma;
+}
+numtoroman('MMCXVII');
+
+
