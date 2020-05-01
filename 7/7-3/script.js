a70774798d3b635a9016142318f5c01402af823d
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-05-01 17:32:13 -0300

    exo done

--- 7/7-3/script.js
+++ 7/7-3/script.js
@@ -1,13 +1,20 @@
 window.onload = function () {
-var cardavailable = ['cinco-de-ouros.png', 'nove-de-espadas.png'];
+const cardavailable = ['tres-de-ouros', 
+'sete-de-paus', 'sete-de-espadas', 'seis-de-copas', 
+'quatro-de-paus', 'cinco-de-ouros', 'nove-de-espadas'];
+const listAnimation  = ['rotation', 'scale', 'skew'];
 // function to add a new card in the grid
 const clickCard = document.querySelector('#reverseCard');
 clickCard.addEventListener('click', function () {
   const randomCard = cardavailable[Math.floor(Math.random() * cardavailable.length)];
- // console.log('randomBall', randomBall);
+  const randomAnimation = listAnimation[Math.floor(Math.random() * listAnimation.length)];
+  // console.log('randomBall', randomBall);
   const grid = document.querySelector('.grid');
   const img = document.createElement('img');
   grid.appendChild(img);
-  img.src = `./cartas/${randomCard}`;
+  img.src = `./cartas/${randomCard}.png`;
+  img.style.height = "200px";
+  img.style.marginRight = '15px';
+  img.className = randomAnimation;
 });
 }//End window.onload
