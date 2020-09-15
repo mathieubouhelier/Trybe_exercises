0836de74cd4970e297ee4a1a476c54b21374100d
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-09-15 15:58:16 -0300

    exo2

---
+++ 25/25-1/exo2.js
@@ -0,0 +1,15 @@
+db.clientes.aggregate([
+  {
+    $match: {
+      $and: [
+        { sexo: 'FEMININO' },
+        {
+          dataNascimento: {
+            $gte: ISODate('1995-04-29T00:00:00.000Z'),
+            $lte: ISODate('2005-07-29T00:00:00.000Z'),
+          },
+        },
+      ],
+    },
+  },
+]);
