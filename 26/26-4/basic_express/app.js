b3bf2e7c9230111884b1ae0f5ee37c9c4d148f7b
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-09-25 05:45:08 -0300

    exo1

--- 26/26-4/basic_express/app.js
+++ 26/26-4/basic_express/app.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req,res) =>{
-  res.send("were are here")
+app.get('/ping', (req,res) =>{
+  res.send("pong")
 })
 
 app.listen(3000);
